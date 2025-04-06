7ba4ee04f8d74a3d18f43c119c61f916a2bf6a37
Gillbert Wong <66062290+gillwong@users.noreply.github.com>
2025-04-06 18:42:49 +0800

    fix: update ansible roles for proxmox

--- README.md
+++ README.md
@@ -1,1 +1,96 @@
 # Homelab
+
+## Introduction
+
+A collection of infrastructure-as-code (IaC) and configuration-as-code (CaC) files for homelab setup.
+
+[OpenTofu](https://opentofu.org) (an open-source fork of [Terraform](https://developer.hashicorp.com/terraform)) is used to provision infrastructure in [Proxmox Virtual Environment (VE)](https://www.proxmox.com/en/products/proxmox-virtual-environment/overview) through IaC while [Vagrant](https://developer.hashicorp.com/vagrant) is used for testing infrastructure configurations.
+
+[Ansible](https://docs.ansible.com) is used for configuring the infrastructure after provisioning through CaC.
+
+## Getting Started
+
+The homelab can be configured via Vagrant only or OpenTofu/Terraform only (either is sufficient). Both tools can be installed to utilize Vagrant for testing infrastructure configurations and OpenTofu/Terraform for the deployed infrastructure. Ansible must be installed in all setups.
+
+### Prerequisites
+
+1. Install IaC tools: OpenTofu ([guide](https://opentofu.org/docs/intro/install/)) or Terraform ([guide](https://developer.hashicorp.com/terraform/install)), and Vagrant ([guide](https://developer.hashicorp.com/vagrant/install))
+
+2. Install CaC tools: Ansible ([guide](https://docs.ansible.com/ansible/latest/getting_started/get_started_ansible.html#get-started-ansible)). There are `requirements.txt` and `.python-version` files under the provisioning directory if Ansible is installed via [pip](https://pypi.org/project/pip/)
+
+### Setup IaC
+
+1. Install [Vagrant Libvirt](https://vagrant-libvirt.github.io/vagrant-libvirt/) using [this guide](https://vagrant-libvirt.github.io/vagrant-libvirt/installation.html)
+
+2. Setup a Proxmox VE server ([guide](https://www.proxmox.com/en/products/proxmox-virtual-environment/get-started)) and follow [this guide](https://github.com/Telmate/terraform-provider-proxmox/blob/master/docs/index.md) to setup the Terraform Proxmox VE provider
+
+3. Create a Proxmox VE VM template using [this guide](https://github.com/Telmate/terraform-provider-proxmox/blob/master/docs/guides/cloud-init%20getting%20started.md). The homelab uses [AlmaLinux OS](https://almalinux.org) 9.5 (an open-source Linux distribution binary compatible with [RHEL](https://www.redhat.com/en/technologies/linux-platforms/enterprise-linux)) and ID 900 for the VM template by default. Get the [cloud-init](https://cloud-init.io) images for AlmaLinux [here](https://wiki.almalinux.org/cloud/Generic-cloud.html). Replace the snippet provided in the guide by running:
+
+```bash
+tee /var/lib/vz/snippets/qemu-guest-agent.yml <<EOF
+#cloud-config
+runcmd:
+  - dnf upgrade -y
+  - dnf install -y qemu-guest-agent python
+  - systemctl start qemu-guest-agent
+EOF
+```
+
+4. View the `prod.tf` file and make changes as necessary, e.g., the Proxmox VE API URL for the provider, network and disk settings for the VM, etc. Optionally, create a `.tfvars` file to store variables
+
+### Setup CaC
+
+1. Add a generic password to the macOS keychain:
+
+```bash
+security add-generic-password -s homelab-playbooks -a ansible-vault -w
+```
+
+2. Replace all `vault.yaml` files. Run the following to encrypt the files:
+
+```bash
+ansible-vault encrypt vault.yaml
+```
+
+3. View the `hosts-prod.yaml` and make changes as necessary
+
+### Provisioning
+
+1. Provision infrastructure using OpenTofu (replace `tofu` with `terraform` if using Terraform):
+
+```bash
+cd infra
+tofu init
+tofu plan -var-file=prod.tfvars -out=plan0
+tofu apply plan0
+cd ../provisioning
+ansible-playbook playbooks/gitlab_runner.yaml --inventory=hosts-prod.yaml
+ansible-playbook playbooks/k8s.yaml --inventory=hosts-prod.yaml
+```
+
+Or using Vagrant:
+
+```bash
+cd infra/test/gitlab_runner
+vagrant up
+cd ../k8s
+vagrant up
+```
+
+### Destroying
+
+1. Destroy infrastructure using OpenTofu (replace `tofu` with `terraform` if using Terraform):
+
+```bash
+cd infra
+tofu destroy
+```
+
+Or using Vagrant:
+
+```bash
+cd infra/test/gitlab_runner
+vagrant destroy
+cd ../k8s
+vagrant destroy
+```
--- infra/prod.tf
+++ infra/prod.tf
@@ -21,19 +21,32 @@ variable "ci_sshkey" {
   nullable    = false
 }
 
+variable "k8s_config" {
+  description = "Kubernetes cluster nodes configuration"
+  type = object({
+    control_plane_nodes = number
+    worker_nodes = number
+  })
+  default = {
+    control_plane_nodes = 3
+    worker_nodes = 3
+  }
+  nullable = false
+}
+
 provider "proxmox" {
-  pm_api_url  = "https://pve.gillwong.com:8006/api2/json"
+  pm_api_url  = "https://pve.gillwong.com:8006/api2/json" # URL to the Proxmox VE server
   pm_user     = "terraform-prov@pve"
   pm_password = var.pm_password
 }
 
 resource "proxmox_vm_qemu" "gitlab-runner" {
-  vmid             = 100
+  vmid             = 110
   name             = "tf-gitlab-runner"
   target_node      = "pve"
   agent            = 1
-  cores            = 4
-  memory           = 8192
+  cores            = 2
+  memory           = 4096
   boot             = "order=scsi0"                             # has to be the same as the OS disk of the template
   clone            = "AlmaLinux-9-5-20241120-x86-64-cloudinit" # The name of the template
   scsihw           = "virtio-scsi-single"
@@ -62,6 +75,126 @@ resource "proxmox_vm_qemu" "gitlab-runner" {
           storage = "local-lvm"
           # The size of the disk should be at least as big as the disk in the template. If it's smaller, the disk will be recreated
           size = "32G"
+          discard = true
+          emulatessd = true
+          iothread = true
+        }
+      }
+    }
+    ide {
+      # Some images require a cloud-init disk on the IDE controller, others on the SCSI or SATA controller
+      ide1 {
+        cloudinit {
+          storage = "local-lvm"
+        }
+      }
+    }
+  }
+
+  network {
+    id     = 0
+    bridge = "vmbr0"
+    model  = "virtio"
+  }
+}
+
+resource "proxmox_vm_qemu" "k8s-cp" {
+  count = var.k8s_config.control_plane_nodes
+  
+  vmid             = 121 + count.index
+  name             = "tf-k8s-cp-${count.index + 1}"
+  target_node      = "pve"
+  agent            = 1
+  cores            = 4
+  memory           = 4096
+  boot             = "order=scsi0"                             # has to be the same as the OS disk of the template
+  clone            = "AlmaLinux-9-5-20241120-x86-64-cloudinit" # The name of the template
+  scsihw           = "virtio-scsi-single"
+  vm_state         = "running"
+  automatic_reboot = true
+
+  # Cloud-Init configuration
+  cicustom   = "vendor=local:snippets/almalinux-template.yml" # /var/lib/vz/snippets/qemu-guest-agent-dnf.yml
+  ciupgrade  = true
+  nameserver = "192.168.0.1" # router IP
+  ipconfig0  = "ip=192.168.1.${count.index + 21}/16,gw=192.168.0.1" # gateway set to router IP
+  skip_ipv6  = true
+  ciuser     = "almalinux"
+  sshkeys    = var.ci_sshkey
+
+  # Most cloud-init images require a serial device for their display
+  serial {
+    id = 0
+  }
+
+  disks {
+    scsi {
+      scsi0 {
+        # We have to specify the disk from our template, else Terraform will think it's not supposed to be there
+        disk {
+          storage = "local-lvm"
+          # The size of the disk should be at least as big as the disk in the template. If it's smaller, the disk will be recreated
+          size = "32G"
+          discard = true
+          emulatessd = true
+          iothread = true
+        }
+      }
+    }
+    ide {
+      # Some images require a cloud-init disk on the IDE controller, others on the SCSI or SATA controller
+      ide1 {
+        cloudinit {
+          storage = "local-lvm"
+        }
+      }
+    }
+  }
+
+  network {
+    id     = 0
+    bridge = "vmbr0"
+    model  = "virtio"
+  }
+}
+
+resource "proxmox_vm_qemu" "k8s" {
+  count = var.k8s_config.worker_nodes
+  
+  vmid             = 121 + var.k8s_config.control_plane_nodes + count.index
+  name             = "tf-k8s-${count.index + 1}"
+  target_node      = "pve"
+  agent            = 1
+  cores            = 4
+  memory           = 4096
+  boot             = "order=scsi0"                             # has to be the same as the OS disk of the template
+  clone            = "AlmaLinux-9-5-20241120-x86-64-cloudinit" # The name of the template
+  scsihw           = "virtio-scsi-single"
+  vm_state         = "running"
+  automatic_reboot = true
+
+  # Cloud-Init configuration
+  cicustom   = "vendor=local:snippets/almalinux-template.yml" # /var/lib/vz/snippets/qemu-guest-agent-dnf.yml
+  ciupgrade  = true
+  nameserver = "192.168.0.1" # router IP
+  ipconfig0  = "ip=192.168.1.${count.index + 21 + var.k8s_config.control_plane_nodes}/16,gw=192.168.0.1" # gateway set to router IP
+  skip_ipv6  = true
+  ciuser     = "almalinux"
+  sshkeys    = var.ci_sshkey
+
+  # Most cloud-init images require a serial device for their display
+  serial {
+    id = 0
+  }
+
+  disks {
+    scsi {
+      scsi0 {
+        # We have to specify the disk from our template, else Terraform will think it's not supposed to be there
+        disk {
+          storage = "local-lvm"
+          # The size of the disk should be at least as big as the disk in the template. If it's smaller, the disk will be recreated
+          size = "64G"
           discard = true
           emulatessd = true
           iothread = true
